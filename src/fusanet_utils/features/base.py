c3e20fbae6ca71c2b09038eb6f88d17a39f5b95f
Pablo Huijse <pablo.huijse@gmail.com>
2021-10-13 18:33:48 -0300

    Features are created outside the datasets directory

--- src/fusanet_utils/features/base.py
+++ src/fusanet_utils/features/base.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from os.path import isfile, splitext
+import pathlib
 import torch
 
 from .waveform import get_waveform
@@ -14,20 +15,24 @@ class Feature(ABC):
     def compute(self, waveform: torch.Tensor):
         pass
 
-    def create_path(self, waveform_path: str) -> str:
+    def create_path(self, waveform_path: pathlib.Path) -> pathlib.Path:
         feature_name = type(self).__name__
-        return splitext(waveform_path)[0]+"_"+feature_name+".pt" 
+        file_name = waveform_path.stem + "_" + feature_name + ".pt"
+        pre_path = pathlib.Path(*waveform_path.parts[:-4])
+        pos_path = pathlib.Path(*waveform_path.parts[-3:-1])
+        (pre_path / "features" / pos_path).mkdir(parents=True, exist_ok=True)
+        return pre_path / "features" / pos_path / file_name
     
     def write_to_disk(self, waveform_path: str) -> None:
-        feature_path = self.create_path(waveform_path)
-        if not isfile(feature_path) or self.params["overwrite"]: 
+        feature_path = self.create_path(pathlib.Path(waveform_path))
+        if not feature_path.exists() or self.params["overwrite"]: 
             waveform =  get_waveform(waveform_path, self.params)
             feature = self.compute(waveform)
             torch.save(feature, feature_path)
 
     def read_from_disk(self, waveform_path: str) -> torch.Tensor:
-        feature_path = self.create_path(waveform_path)
-        if isfile(feature_path):
+        feature_path = self.create_path(pathlib.Path(waveform_path))
+        if feature_path.exists():
             return torch.load(feature_path)
         else:
             raise FileNotFoundError("Feature file not found")
