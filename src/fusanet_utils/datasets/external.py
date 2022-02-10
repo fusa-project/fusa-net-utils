e4b2f3b7bee205c39246de2d5afb8178f40c0c7f
Pablo Huijse <pablo.huijse@gmail.com>
2022-02-10 14:20:38 -0300

    Create base label dataset class to facilitate the creation of new datasets

--- src/fusanet_utils/datasets/external.py
+++ src/fusanet_utils/datasets/external.py
@@ -1,132 +1,120 @@
-from os import listdir
-from os.path import join, isfile
 from pathlib import Path
 import logging
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Union
 import pandas as pd
 from torch.utils.data import Dataset
+from abc import abstractmethod
 
 logger = logging.getLogger(__name__)
 
-def get_label_transforms(repo_path: str, dataset_name: str) -> Dict:
-    taxonomy_path = join(repo_path, "fusa_taxonomy.json")   
-    a = pd.read_json(taxonomy_path).T[dataset_name].to_dict()
-    transforms = {}
-    for key, values in a.items():
-        for value in values:
-            transforms[value] = key
-    return transforms
+class FolderDataset(Dataset):
 
-class ExternalDataset(Dataset):
-
+    def __init__(self, folder_path: Union[str, Path]):
+        """
+        Expects a folder with audio files
+        """
+        if isinstance(folder_path, str):
+            folder_path = Path(folder_path)
+        self.file_list = list(folder_path.glob( '*' ))        
+        self.categories = ['dummy']
+        self.labels = self.categories*len(self.file_list)
+    
     def __getitem__(self, idx: int) -> Tuple[str, int]:
-        return (self._file_path(idx), self.labels[idx])
+        return (self.file_list[idx], self.labels[idx])
         
     def __len__(self) -> int:        
         return len(self.file_list)
 
-class FolderDataset(ExternalDataset):
+class LabeledDataset(Dataset):
 
-    def __init__(self, folder_path: str):
-        """
-        Expects a folder with audio files
-        """
-        self.audio_path = folder_path
-        self.file_list = sorted(listdir(self.audio_path))
-        self.labels = ['dummy']*len(self.file_list)
-        self.categories = ['dummy']
-
-    def _file_path(self, idx: int) -> str:
-        return join(self.audio_path, self.file_list[idx])
-
-
-
-class ESC(ExternalDataset):
-
-    def __init__(self, repo_path: str):
-        
+    def __init__(self, repo_path: Union[str, Path], dataset_name: str, audio_rel_path: Path, metadata_rel_path: Path):
+        if isinstance(repo_path, str):
+            repo_path = Path(repo_path)
+        self.repo_path = repo_path
         # TODO: Abstract parts of init
-        label_transforms = get_label_transforms(repo_path, "ESC")
-        datasets_path = join(repo_path, "datasets")        
-        df = pd.read_csv(join(datasets_path, "ESC-50", "meta", "esc50.csv"))
-        ESC_classes = df["category"].unique()
-        # Verify that there are no typos in FUSA_taxonomy
-        if not all([key in set(ESC_classes) for key in label_transforms.keys() if key != ""]):
-            logger.warning("Existen llaves de ESC que no calzan en fusa_taxonomy.json")
-        
-        self.audio_path = join(datasets_path, "ESC-50", "audio")
-        # Verify that files exist
-        file_exist = df["filename"].apply(lambda x: isfile(join(datasets_path, "ESC-50", "audio", x)))
-        if not file_exist.all():
-            logger.warning("Existen rutas incorrectas o archivos perdidos")
-            df = df.loc[file_exist]
-        
-        self.file_list, self.labels, self.categories = [], [], []
-        for label in ESC_classes:
-            if label in label_transforms:
-                self.categories += [label_transforms[label]]
-                mask = df["category"] == label
-                self.file_list += list(df["filename"].loc[mask])
-                self.labels += [label_transforms[label]]*sum(mask)
-
-    def _file_path(self, idx: int) -> str:
-        return join(self.audio_path, self.file_list[idx])
-
-
-class UrbanSound8K(ExternalDataset):
-    
-    def __init__(self, repo_path: str):
-        label_transforms = get_label_transforms(repo_path, "UrbanSound")
-        datasets_path = join(repo_path, "datasets")
-        df = pd.read_csv(join(datasets_path, "UrbanSound8K", "metadata", "UrbanSound8K.csv"))
-        self.audio_path = join(datasets_path, "UrbanSound8K", "audio")
-        self.file_list = []
-        self.fold_list = []
-        self.labels = []
-        self.categories = []
-        for label in df["class"].unique():
-            if label in label_transforms:
-                self.categories += [label_transforms[label]]
-                mask = (df["class"] == label) & (df["end"] - df["start"] >= 0.7)
-                self.file_list += list(df["slice_file_name"].loc[mask])
-                self.fold_list += list(df["fold"].loc[mask])
-                self.labels += [label_transforms[label]]*sum(mask)
-        self.fold_list = ['fold' + str(fold) for fold in self.fold_list]
-
-
-    def _file_path(self, idx: int) -> str:
-        return join(self.audio_path, self.fold_list[idx], self.file_list[idx])
-
-class VitGlobal(ExternalDataset):
-    
-    def __init__(self, repo_path: str):
-        label_transforms = get_label_transforms(repo_path, "Vitglobal")
-        datasets_path = join(repo_path, "datasets")
-        df = pd.read_csv(join(datasets_path, "VitGlobal", "meta", "audios_eruido2022_20220121.csv"))
-        self.audio_path = join(datasets_path, "VitGlobal", "audio", "dataset")
-
-        classes = df["WAVE_MEMO"].unique()
+        datasets_path = repo_path / "datasets"
+        self.audio_prefix_path = datasets_path / audio_rel_path
+        file_paths, labels = self.parse_metadata(datasets_path / metadata_rel_path)
+        classes = labels.unique()
+        label_transforms = self.get_label_transforms(dataset_name)
         # Verify that there are no typos in FUSA_taxonomy
         if not all([key in set(classes) for key in label_transforms.keys() if key != ""]):
-            logger.warning("Existen llaves de ESC que no calzan en fusa_taxonomy.json")
+            logger.warning(f"Existen llaves de {dataset_name} que no calzan en fusa_taxonomy.json")
         
-        self.audio_path = Path(datasets_path) / "VitGlobal" / "audio" / "dataset"
-        # Verify that files exist
-        #import ipdb; ipdb.set_trace()
-        file_names = df["WAVE_URL"].apply(lambda x: Path(x).stem +'.mp3')
-        file_exist = file_names.apply(lambda x: (self.audio_path / Path(x)).exists())
+        # Verify that all files exist
+        file_exist = file_paths.apply(lambda file: file.exists())
         if not file_exist.all():
-            logger.warning("Existen rutas incorrectas o archivos perdidos en la metadata")
-            df = df.loc[file_exist]
-        file_names = file_names[file_exist]
-
+            logger.warning("Existen rutas incorrectas o archivos perdidos")
+            file_paths = file_paths.loc[file_exist]
+            labels = labels.loc[file_exist]
+        
         self.file_list, self.labels, self.categories = [], [], []
         for label in classes:
             if label in label_transforms:
                 self.categories += [label_transforms[label]]
-                mask = df["WAVE_MEMO"] == label
-                self.file_list += list(file_names.loc[mask])
+                mask = labels == label
+                self.file_list += list(file_paths.loc[mask])
                 self.labels += [label_transforms[label]]*sum(mask)
 
-    def _file_path(self, idx: int) -> str:
-        return join(self.audio_path, self.file_list[idx])
+    
+    def get_label_transforms(self, dataset_name: str) -> Dict:
+        taxonomy_path = self.repo_path / "fusa_taxonomy.json"
+        a = pd.read_json(taxonomy_path).T[dataset_name].to_dict()
+        transforms = {}
+        for key, values in a.items():
+            for value in values:
+                transforms[value] = key
+        return transforms
+
+    @abstractmethod
+    def parse_metadata(self, metadata_path: Path) -> Tuple:
+        """
+        Returns a tuple of a series with the paths and a series with the labels
+        """
+        pass
+
+    def __getitem__(self, idx: int) -> Tuple[Path, int]:
+        return (self.file_list[idx], self.labels[idx])
+        
+    def __len__(self) -> int:        
+        return len(self.file_list)
+
+
+class ESC(LabeledDataset):
+
+    def __init__(self, repo_path: Union[str, Path]):
+        super().__init__(repo_path, dataset_name="ESC", audio_rel_path=Path("ESC-50") / "audio", metadata_rel_path=Path("ESC-50") / "meta" / "esc50.csv")
+
+    def parse_metadata(self, metadata_path: Path) -> Tuple:
+        df = pd.read_csv(metadata_path)
+        file_names = df["filename"]
+        labels = df["category"]
+        file_paths = file_names.apply(lambda file_name: self.audio_prefix_path / file_name)
+        return file_paths, labels
+
+class UrbanSound8K(LabeledDataset):
+
+    def __init__(self, repo_path: Union[str, Path]):
+        super().__init__(repo_path, dataset_name="UrbanSound", audio_rel_path=Path("UrbanSound8K") / "audio", metadata_rel_path=Path("UrbanSound8K") / "metadata" / "UrbanSound8K.csv")
+
+    def parse_metadata(self, metadata_path: Path) -> Tuple:
+        df = pd.read_csv(metadata_path)
+        mask = df["end"] - df["start"] >= 0.7
+        df = df.loc[mask]
+        file_folds = df["fold"].apply(lambda x: Path(f'fold{x}')) 
+        file_names = df["slice_file_name"].apply(lambda x: Path(x))
+        labels = df["class"]
+        file_paths = (file_folds / file_names).apply(lambda file_name: self.audio_prefix_path / file_name)
+        return file_paths, labels
+
+class VitGlobal(LabeledDataset):
+
+    def __init__(self, repo_path: Union[str, Path]):
+        super().__init__(repo_path, dataset_name="Vitglobal", audio_rel_path=Path("VitGlobal") / "audio" / "dataset", metadata_rel_path=Path("VitGlobal") / "meta" / "audios_eruido2022_20220121.csv")
+
+    def parse_metadata(self, metadata_path: Path) -> Tuple:
+        df = pd.read_csv(metadata_path)
+        file_names = df["WAVE_URL"].apply(lambda x: Path(x).stem +'.mp3')
+        labels = df["WAVE_MEMO"]
+        file_paths = file_names.apply(lambda file_name: self.audio_prefix_path / file_name)
+        return file_paths, labels
