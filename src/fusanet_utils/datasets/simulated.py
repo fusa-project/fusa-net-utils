fc4272dfc599326c0bb69514fcad15f54ecbde39
Pablo Huijse <pablo.huijse@gmail.com>
2022-04-14 17:46:55 -0400

    SimulatedPoliphonic returns the labels as DataFrames

--- src/fusanet_utils/datasets/simulated.py
+++ src/fusanet_utils/datasets/simulated.py
@@ -1,17 +1,12 @@
 from pathlib import Path
 import logging
-from typing import Union, Tuple, Dict, List
+from typing import Union, Tuple, List
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-import torch
 from torch.utils.data import Dataset
 
-from ..features.waveform import get_waveform
 
 logger = logging.getLogger(__name__)
-
-def create_sed_labels(labels: List[int], start_times: List[float], end_times: List[float], audio_length:int, sampling_frequency:int=32000, window_size:int=1000):
-    pass
 
 
 class SimulatedPoliphonic(Dataset):
@@ -19,9 +14,10 @@ class SimulatedPoliphonic(Dataset):
     def __init__(self, repo_path: Union[str, Path], mini: bool=True):
         if isinstance(repo_path, str):
             repo_path = Path(repo_path)
- 
-        dataset_path = repo_path / "datasets" / "Poliphonic mono mini"
-        features_path = repo_path / "features" / "Poliphonic mono mini"
+        if mini: 
+            dataset_path = repo_path / "datasets" / "Poliphonic mono mini"
+        else:
+            dataset_path = repo_path / "datasets" / "Poliphonic mono"
         self.categories = []
         self.file_list = []
         self.label_list = []
@@ -30,24 +26,17 @@ class SimulatedPoliphonic(Dataset):
             df = pd.read_csv(file, sep=',', header=0)
             self.categories.append(df["class"].unique)
         self.categories = sorted(list(set(self.categories)))
-        n_classes = len(self.categories)
         self.le = LabelEncoder().fit(self.categories)
         
         for file in (dataset_path / "meta").glob("*.csv"):
             df = pd.read_csv(file, sep=',', header=0)
             for audio_file, metadata in df.groupby(by="audio_filename"):
                 audio_path = dataset_path / "audio" / audio_file
-                audio = get_waveform(audio_path, feature_params)
                 self.file_list.append(audio_path)
-                label = create_sed_labels()
-                label_path = features_path / audio_file.stem + "_label.pt"
-                with open(label_path, "wb") as f:
-                    torch.save(label, f)
-                self.label_list.append(label_path)
-            break
+                self.label_list.append(metadata)
 
 
-    def __getitem__(self, idx: int) -> Tuple[Path, Path]:
+    def __getitem__(self, idx: int) -> Tuple[Path, pd.DataFrame]:
         return (self.file_list[idx], self.label_list[idx])
 
     def __len__(self) -> int:
