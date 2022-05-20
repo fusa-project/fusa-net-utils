66c9a13a2e306061b8c5ecdc41fa8e39ba757cb7
Pablo Huijse <pablo.huijse@gmail.com>
2022-05-20 11:20:06 -0400

    Create label matrix for SED (hardcoded)

--- src/fusanet_utils/datasets/simulated.py
+++ src/fusanet_utils/datasets/simulated.py
@@ -4,7 +4,7 @@ from typing import Union, Tuple, List
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from torch.utils.data import Dataset
-
+import torch
 
 logger = logging.getLogger(__name__)
 
@@ -34,8 +34,18 @@ class SimulatedPoliphonic(Dataset):
             for audio_file, metadata in df.groupby(by="audio_filename"):
                 audio_path = dataset_path / "audios" / audio_file
                 self.file_list.append(audio_path)
-                self.label_list.append(metadata)
-
+                audio_seconds = 10 # TODO: Read this from data
+                # audio_samples = 44100*audio_seconds
+                audio_windows = 1001 # audio_samples // 1000 # TODO: Calculate this from params (target sampling freq, number of windows)
+                label = torch.zeros(audio_windows, len(self.categories)) 
+                label_idx = self.le.transform(list(metadata['class'])).astype('int')
+                start_norm, end_norm = (audio_windows/audio_seconds)*metadata[['start (s)', 'end (s)']].values.T
+                start_idx = start_norm.astype('int')
+                end_idx = end_norm.astype('int')
+                for k, entity in enumerate(label_idx): # TODO: Make this more efficient
+                    label[start_idx[k]:end_idx[k], entity] = 1.
+                self.label_list.append(label)
+            
 
     def __getitem__(self, idx: int) -> Tuple[Path, pd.DataFrame]:
         return (self.file_list[idx], self.label_list[idx])
