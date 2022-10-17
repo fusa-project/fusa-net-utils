425cc19a50fce67237e9264091adb5bd3f977263
Pablo Huijse <pablo.huijse@gmail.com>
2022-10-17 18:15:51 -0300

    white noise transform

--- src/fusanet_utils/transforms.py
+++ src/fusanet_utils/transforms.py
@@ -6,50 +6,81 @@ from typing import Dict, List
 
 logger = logging.getLogger(__name__)
 
+
+class WhiteNoise(object):
+
+    def __init__(self, SNR_min=-1, SNR_max=1):
+        self.SNR_min = SNR_min
+        self.SNR_max = SNR_max
+
+    def __call__(self, sample, snr=None):
+        waveform = sample['waveform']
+        if snr is None:  # Random from SNR range
+            snr = self.SNR_min + torch.rand(1).item()*(self.SNR_max - self.SNR_min)
+        assert waveform.shape[0] == 1, "Only use for mono audio"
+        signal_power = torch.sum(waveform**2)
+        noise_power = signal_power/(10**snr)
+        noise = torch.randn_like(waveform)*noise_power
+        noisy_waveform = waveform + noise
+        sample['waveform'] = noisy_waveform/noisy_waveform.abs().amax()
+        return sample
+
+
+def test_white_noise():
+    waveform = torch.randn(1, 1000)
+    sample = {}
+    sample['waveform'] = waveform
+    sample['label'] = 'asd'
+    transformed_sample = WhiteNoise()(sample)
+    return transformed_sample
+
+
 def crop(data, target_length, collate_dim):
     if data.size(collate_dim) > target_length:
-        if data.ndim == 2: 
-            if collate_dim == -1: # audio
-                data = data[:,:target_length]
-            elif collate_dim == -2: # sed label
-                data = data[:target_length, :] 
-        elif data.ndim == 3: # spectrogram
-            data = data[:,:,:target_length]
+        if data.ndim == 2:
+            if collate_dim == -1:  # audio
+                data = data[:, :target_length]
+            elif collate_dim == -2:  # sed label
+                data = data[:target_length, :]
+        elif data.ndim == 3:  # spectrogram
+            data = data[:, :, :target_length]
         else:
             raise NotImplementedError("Only 3D o 4D tensors accepted")
     return data
+
 
 def append(data, target_length, collate_dim):
     if data.size(collate_dim) < target_length:
         append_length = target_length-data.size(collate_dim)
         if data.ndim == 2:
-            if collate_dim == -1: # audio
+            if collate_dim == -1:  # audio
                 data = pad(data, (0, append_length, 0, 0), mode='constant', value=0)
-            elif collate_dim == -2: #sed label
+            elif collate_dim == -2:  # sed label
                 print(data.shape)
-                data =  pad(data, (0, 0, 0, append_length), mode='constant', value=0)
+                data = pad(data, (0, 0, 0, append_length), mode='constant', value=0)
                 print(data.shape)
-        elif data.ndim == 3: # spectrogram
+        elif data.ndim == 3:  # spectrogram
             data = pad(data, (0, append_length, 0, 0, 0, 0), mode='constant', value=0)
         else:
             raise NotImplementedError("Only 3D o 4D tensors accepted")
     return data
 
+
 class Collate_and_transform:
-    
-    def __init__(self, params: Dict, transforms: List=[]):
+
+    def __init__(self, params: Dict, transforms: List = []):
         self.transforms = transforms
         self.resizer = params['collate_resize']
-        
-    def __call__(self, batch: List[Dict]) -> Tensor:
+
+    def __call__(self, batch: List[Dict]) -> Dict:
         data_keys = list(batch[0].keys())
-        if batch[0]['label'].ndim == 1: # TAG
+        if batch[0]['label'].ndim == 1:  # TAG
             data_keys.remove('label')
         data_keys.remove('filename')
         for key in data_keys:
             if not self.resizer == 'none':
                 if key == 'label' or key == 'distance':
-                    collate_dim = -2 
+                    collate_dim = -2
                 else:
                     collate_dim = -1
                 lens = [sample[key].size(collate_dim) for sample in batch]
@@ -60,7 +91,6 @@ class Collate_and_transform:
                 elif self.resizer == 'crop':
                     for sample in batch:
                         sample[key] = crop(sample[key], min(lens), collate_dim)
-                
         # Data augmentation transforms
         transformed_batch = []
         for sample in batch:
@@ -68,12 +98,11 @@ class Collate_and_transform:
                 sample = transform(sample)
             transformed_batch.append(sample)
         mbatch = {}
-        if sample['label'].ndim == 1: # TAG
+        if sample['label'].ndim == 1:  # TAG
             mbatch['label'] = torch.LongTensor([sample['label'] for sample in transformed_batch])
-        else: # SED
-            mbatch['label'] =  torch.stack([sample['label'] for sample in transformed_batch], dim=0) 
+        else:  # SED
+            mbatch['label'] = torch.stack([sample['label'] for sample in transformed_batch], dim=0)
         mbatch['filename'] = [sample['filename'] for sample in transformed_batch]
         for key in data_keys:
-            mbatch[key] =  torch.stack([sample[key] for sample in transformed_batch], dim=0)
+            mbatch[key] = torch.stack([sample[key] for sample in transformed_batch], dim=0)
         return mbatch
-
