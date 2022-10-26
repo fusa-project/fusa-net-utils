2c01af089e93fe8cfbc6348d255583a66a1518fe
Pablo Huijse <pablo.huijse@gmail.com>
2022-10-26 17:57:34 -0300

    fix wrong argument name for colored noise

--- src/fusanet_utils/augmentations/additive_noise.py
+++ src/fusanet_utils/augmentations/additive_noise.py
@@ -5,18 +5,22 @@ import colorednoise as cn
 import librosa
 from typing import Dict
 
+
 class Noise(ABC):
-    def __init__(self, SNR_min: float=-1., SNR_max: float=1.):
+    def __init__(self, SNR_min: float = -1.0, SNR_max: float = 1.0, snr=None):
         self.SNR_min = SNR_min
         self.SNR_max = SNR_max
+        self.snr = snr
 
     @abstractmethod
-    def generate_noise(self, n_samples: int, random_state=None) -> np.array:
+    def generate_noise(self, n_samples: int, random_state=None):
         pass
 
-    def __call__(self, sample: Dict, snr=None, random_state=None):
-        if snr is None:
+    def __call__(self, sample: Dict, random_state=None):
+        if self.snr is None:
             snr = self.SNR_min + torch.rand(1).item()*(self.SNR_max - self.SNR_min)
+        else:
+            snr = self.snr
         signal = sample['waveform']
         assert signal.shape[0] == 1, "Only use for mono audio"
         signal_rms = np.mean(librosa.feature.rms(y=signal[0, :]))
@@ -37,15 +41,18 @@ class WhiteNoise(Noise):
 class PinkNoise(Noise):
 
     def generate_noise(self, n_samples: int, random_state=None) -> torch.Tensor:
-        noise = cn.powerlaw_psd_gaussian(beta=1, samples=n_samples, fmin=0, random_state=random_state)
+        noise = cn.powerlaw_psd_gaussian(exponent=1.0, size=n_samples, fmin=0,
+                                         random_state=random_state)
         return torch.from_numpy(noise).reshape(1, -1)
 
 
 class RedNoise(Noise):
 
     def generate_noise(self, n_samples: int, random_state=None) -> torch.Tensor:
-        noise = cn.powerlaw_psd_gaussian(beta=2, samples=n_samples, fmin=0, random_state=random_state)
+        noise = cn.powerlaw_psd_gaussian(exponent=2.0, size=n_samples, fmin=0,
+                                         random_state=random_state)
         return torch.from_numpy(noise).reshape(1, -1)
+
 
 def test_white_noise():
     waveform = torch.randn(1, 1000)
@@ -54,5 +61,3 @@ def test_white_noise():
     sample['label'] = 'asd'
     transformed_sample = WhiteNoise()(sample)
     return transformed_sample
-
-
