2575219054fe479d8a8e7a5fcb2b14f422523d39
Victor Vargas Sandoval <victorvargassandoval93@gmail.com>
2023-08-01 21:46:47 -0400

    Update waveform_backends.py

--- src/fusanet_utils/features/waveform_backends.py
+++ src/fusanet_utils/features/waveform_backends.py
@@ -9,8 +9,10 @@ def read_soundfile(file):
     try:
         samples, origin_sr = sf.read(file)
         samples = samples[:, np.newaxis].astype(np.float32)
+        logger.info("soundfile read sucessfully")
         return samples, origin_sr
-    except:
+    except Exception as error:
+        logger.info("Error en soundfile:", error)
         return None, None
 
 def read_pydub(file):
@@ -19,9 +21,9 @@ def read_pydub(file):
         logger.info("pydub read sucessfully")
         origin_sr = asegment.frame_rate
         channel_sounds = asegment.split_to_mono()
-        logger.debug("channel_sounds len:", len(channel_sounds))
+        logger.info("channel_sounds len:", len(channel_sounds))
         samples = [s.get_array_of_samples() for s in channel_sounds]
-        logger.debug("first array_of_samples:", len(channel_sounds[0].get_array_of_samples()))
+        logger.info("first array_of_samples:", len(channel_sounds[0].get_array_of_samples()))
         logger.info("len of samples :",len(samples))
         # Convert to float32
         fp_arr = np.array(samples).T.astype(np.float32)
@@ -30,4 +32,4 @@ def read_pydub(file):
     except pydub.exceptions.CouldntDecodeError:
         return None, None
     except Exception as error:
-        logger.info("Error :", error)
+        logger.info("Error en pydub:", error)
