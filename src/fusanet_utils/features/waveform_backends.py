0216313470db286e876c00837063db9f2ca33603
Victor Vargas Sandoval <victorvargassandoval93@gmail.com>
2023-08-01 21:50:42 -0400

    Update waveform_backends.py

--- src/fusanet_utils/features/waveform_backends.py
+++ src/fusanet_utils/features/waveform_backends.py
@@ -23,8 +23,8 @@ def read_pydub(file):
         channel_sounds = asegment.split_to_mono()
         logger.info("channel_sounds len:", len(channel_sounds))
         samples = [s.get_array_of_samples() for s in channel_sounds]
-        logger.info("first array_of_samples:", len(channel_sounds[0].get_array_of_samples()))
-        logger.info("len of samples :",len(samples))
+        #logger.info("first array_of_samples:", len(channel_sounds[0].get_array_of_samples()))
+        #logger.info("len of samples :",len(samples))
         # Convert to float32
         fp_arr = np.array(samples).T.astype(np.float32)
         fp_arr /= np.iinfo(samples[0].typecode).max
